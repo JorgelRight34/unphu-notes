a59dd67a9f49817c9079eeb04162609e09430e0c
JorgeRight34 <jorgelright34rd30012003@gmail.com>
2025-04-07 00:11:08 -0400

    Swithcing pc

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdf20472da24f4c90a67c8bfed9f0b1a75564f16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7497e2cc9a900822b7e2299a5fdbf3f4a682743")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
