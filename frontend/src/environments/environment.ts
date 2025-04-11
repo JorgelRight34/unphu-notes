7a753c815508eb40d18b064822cc4c1fdaaa430f
JorgelRight34 <JorgelRight34RD30012003@gmail.com>
2025-04-11 15:02:18 -0400

    Fixed environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   apiUrl:
-    'unphu-notes-backend-eefybdgqgsepbta9.canadacentral-01.azurewebsites.net/',
+    'https://unphu-notes-backend-eefybdgqgsepbta9.canadacentral-01.azurewebsites.net/',
   googleClientId:
     '932306178138-c8sf5ekiepq7cnlbiqjgfn6142pn854s.apps.googleusercontent.com',
 };
