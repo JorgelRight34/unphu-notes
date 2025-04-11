656e29b11b2439527e97fcd4235264a1dc579867
JorgelRight34 <JorgelRight34RD30012003@gmail.com>
2025-04-11 15:07:44 -0400

    Fixed vercel

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   apiUrl:
-    'https://unphu-notes-backend-eefybdgqgsepbta9.canadacentral-01.azurewebsites.net/',
+    'https://unphu-notes-backend-eefybdgqgsepbta9.canadacentral-01.azurewebsites.net/api/',
   googleClientId:
     '932306178138-c8sf5ekiepq7cnlbiqjgfn6142pn854s.apps.googleusercontent.com',
 };
