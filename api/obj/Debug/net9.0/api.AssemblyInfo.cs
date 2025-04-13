556c70fc7096ec592db0465218092d15d71db1e4
Pablo Lorenzo <pabloernesto802@gmail.com>
2025-04-13 12:01:54 -0400

    feat: Added assets folder

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab36018663061aae6c92fb49272571138383b89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c16501bb2fb1fdd4ca614bb2ae3ca743b197f5ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
